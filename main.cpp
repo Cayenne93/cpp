153c52496d6d33f59e6f69026bd7c6761a88d538
Cayenne93 <117767068+Cayenne93@users.noreply.github.com>
2022-11-08 20:21:26 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
-#include <iomanip> //isvesties formatavimui
-#include <cmath>
+
 using namespace std;
+
 int main() {
-    int n,m;
-    cin>>n>>m;
-    int plotas , perim;
-    if(n!=m) //jeigu krastines nelygios
-    {
-        perim=2*n+2*m;
-        cout<<perim;
-    }
-    else{
-        plotas=m*n;
-        cout<<plotas;
-    }
+    int n;
+    cin>>n;
+    if(n>0) cout<<"TAIP";
+    else cout<<"NE" ;
     return 0;
 }
