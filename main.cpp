de1ff8cddfe1fff873568c875ccae180f416406c
Cayenne93 <117767068+Cayenne93@users.noreply.github.com>
2022-11-08 22:10:27 +0200

    7

--- main.cpp
+++ main.cpp
@@ -2,24 +2,10 @@
 
 using namespace std;
 
-bool subtract(int& time, int reset){
-    if (time > 0) {
-        time--;
-        return false;
-    } else {
-        time = reset;
-        return true;
-    }
-}
-
 int main() {
-    int h, m, s;
-    cin >> h >> m >> s;
-
-    if (subtract(s, 59))
-        if (subtract(m, 59))
-            subtract(h, 23);
-
-    cout << h << " " << m << " " << s;
+    int x;
+    cin >> x;
+    if ((x < 100 or x > 999) and x % 66 == 0) cout << "TAIP";
+    else cout << "NE";
     return 0;
 }
